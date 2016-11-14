63ea5e165bdb05544c7da3bf90c36261ed9490e1
hichem <hichem.makhlouf@laratech.com.tn>
2016-11-14 21:20:04 +0100

    commit : Home page

--- smart-bid/src/app/app.module.ts
+++ smart-bid/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './public_user/home/home.compenent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
